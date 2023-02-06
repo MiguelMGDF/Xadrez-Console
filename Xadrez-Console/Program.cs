1b27f13feaaa3e3941a3ae97f0ec0b9070e1ae0f
Miguel Cortez <miguelcortezrj@gmail.com>
2023-02-06 19:01:00 -0300

    Iniciando projeto e criando classe Position

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Board;
+
+namespace Xadrez_Console { 
+    class Program{
+        static void Main(string[] args){
+
+            Position p;
+            p = new Position(3,4);
+            System.Console.WriteLine("Posicao " + p);
+
+            Console.ReadLine();
+
+        }
+    }
+}
